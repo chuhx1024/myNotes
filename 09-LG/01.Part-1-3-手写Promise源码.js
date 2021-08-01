ac5784cd4157f226890138091e8cf35eceb25b51
chuwx <var_chx@126.com>
2021-08-01 17:14:59 +0800

    Feat: Promise done

--- 09-LG/01.Part-1-3-手写Promise源码.js
+++ 09-LG/01.Part-1-3-手写Promise源码.js
@@ -3,17 +3,30 @@ const PENDING = 'pending'
 const FULFILLEN = 'fulfilled'
 const REJECTED = 'rejected'
 
+const resolvePromise = (x, resolve, reject) => {
+    if (x instanceof MyPromise) {
+        x.then(resolve, reject)
+    } else {
+        resolve(x)
+    }
+}
+
 class MyPromise {
+    // 定一个执行器 new Promise 时 立即执行
     constructor (executor) {
         executor(this.resolve, this.reject)
     }
     // Promise 的内部状态 
     status = PENDING
+
     // then方法第一参数传来的成功的值
     value = undefined
     // then方法第二参数传来的失败的原因
     reason = undefined
+
+    // 先存储 then传递的第一个方法  为了异步调用
     successCb = []
+    // 先存储 then传递的第二个方法  为了异步调用
     failCb = []
 
     resolve =(val) => {
@@ -21,6 +34,7 @@ class MyPromise {
         if (this.status !== PENDING) return
         this.status = FULFILLEN
         this.value = val
+        // 使用 while 加 .shift() 的弹出模式 很方便
         while (this.successCb.length) {
             this.successCb.shift()(this.value)
         }
@@ -36,23 +50,65 @@ class MyPromise {
         }
     }
     then (successCb, failCb) {
-        console.log(this.status, 80)
-        switch (this.status) {
-            case FULFILLEN:
-                var x = successCb(this.value)
-                break
-            case REJECTED: 
-                failCb(this.reason)
-                console.log(123)
-                break
-            default: 
-                this.successCb.push(successCb)
-                this.failCb.push(failCb)
-        }
         return new MyPromise((resolve, reject) => {
-            resolve(x)
+            switch (this.status) {
+                case FULFILLEN:
+                    var x = successCb(this.value)
+                    resolvePromise(x, resolve, reject)
+                    break
+                case REJECTED: 
+                    failCb(this.reason)
+                    break
+                default: 
+                    this.successCb.push(successCb)
+                    this.failCb.push(failCb)
+            }
+            
         })
     }
+
+    static all (arr) {
+        let res = []
+        // index 这个思路 可以先占个位置 等最后执行完了  赋值操作都完成了 再返回 res
+        let index = 0
+        return new MyPromise((resolve, reject) => {
+            arr.forEach((item, index) => {
+                if (item instanceof MyPromise) {
+                    item.then(val => {
+                        res[index] = val
+                        index ++ 
+                        if (index === arr.length) {
+                            resolve(res)
+                            console.log(res,2)
+                        }
+                    }, (val) => {
+                        reject(val)
+                    })
+                } else {
+                    res[index] = item
+                    index ++ 
+                    if (index === arr.length) {
+                        resolve(res)
+                        console.log(res,1)
+                    }
+                }
+            })
+        }) 
+    }
+
+    static resolve (val) {
+        if ( val instanceof MyPromise) {
+            return val
+        } else {
+            return new MyPromise((resolve, reject) => {
+                resolve(val)
+                reject()
+            })
+        }
+
+    }
+
+
 
 }
 
@@ -62,23 +118,48 @@ class MyPromise {
 // })
 // console.log(ccc)
 const bbb = new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('123444')
-    }, 1000)
-    setTimeout(() => {
-        reject("999888")
-    }, 2000)
+    // setTimeout(() => {
+        resolve('成功时传入')
+    // }, 1000)
+    // setTimeout(() => {
+        reject("失败了传入")
+    // }, 2000)
 })
+
+function other () {
+    return new MyPromise((resolve, reject) => {
+        resolve('other传入的值')
+    })
+}
 
 console.log(bbb)
 bbb.then((val) => {
-    console.log(val)
-    return 100
+    console.log(val, "这里可以获取成功时传入的值")
+    return bbb
 }, (val) => {
     console.log(val)
 }).then((val) => {
-    console.log(val)
-    console.log('第二')
+    console.log(val, '我是上个 return的值')
 }, (val) => {
+    console.log(val)
+})
+
+const p1 = new MyPromise((resolve, reject) => {
+    resolve('p1成功的值')
+    reject()
+})
+const p2 = new MyPromise((resolve, reject) => {
+    resolve('p2成功的值')
+    reject()
+})
+console.log(p1, 'p1')
+MyPromise.all(['a', 'b', p1, p2, 'c']).then(res => {
+    console.log(res, 'all')
+})
+
+MyPromise.resolve("resolve").then((val) => {
+    console.log(val)
+})
+MyPromise.resolve(p1).then((val) => {
     console.log(val)
 })
