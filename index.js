7edeb2befa8aefa91a9e3ca45ab8fe322d3f98ba
chuwx <var_chx@126.com>
2021-08-02 08:01:23 +0800

    Feat:解构赋值

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 
-const promise = new Promise((resolve, reject) => {
-  foo()
-  resolve(100)
-  reject(new Error('我失败了'))
-})
 
-promise.then(val => {
-  console.log(val, 123)
-}, val => {
-  console.log(val, 888)
-})
