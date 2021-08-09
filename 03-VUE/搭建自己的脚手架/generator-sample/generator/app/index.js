e8c5b358e4b41dd4ad746d5683f82124f971bd80
chuhx <chuhx1024@126.com>
2021-08-09 22:51:41 +0800

    Generator

---
+++ 03-VUE/搭建自己的脚手架/generator-sample/generator/app/index.js
@@ -0,0 +1,21 @@
+// 此文作为 Generator 的核心入口
+// 需要导出一个继承自 Yeoman Generator 的类型
+// Yeoman Generator 在工作是会自动调用我们在此类型中定义的一些生命周期方法
+// 在此文件中 可以通过父类提供的一些方法实现一些功能 如 文件写入
+// 在此文件中 可以通过父类提供的一些方法实现一些功能 如 文件写入
+// 在此文件中 可以通过父类提供的一些方法实现一些功能 如 文件写入
+// 在此文件中 可以通过父类提供的一些方法实现一些功能 如 文件写入
+
+const Generator = require('yeoman-generator')
+
+module.export = class extends Generator {
+    writing () {
+        // Yeoman 自动生成文件阶段自动调用此方法
+        this.fs.write(
+            this.destinationPath(temp.txt), // 写入文件的路径 借助了  this.destinationPath 这个API 方便获取路径
+            '12121212'                      // 写入文件的内容
+        )
+    }
+
+}
+
