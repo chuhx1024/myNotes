f99bf8ea4e85452ae26b83e52b0170c3e5b4c466
chuhx <chuhx1024@126.com>
2021-08-18 20:03:49 +0800

    Webpack

--- 10-大前端/Webpack/webpack-demo/webpack.config.js
+++ 10-大前端/Webpack/webpack-demo/webpack.config.js
@@ -23,7 +23,14 @@ module.exports = {
             },
             {
                 test: /.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    'style-loader',
+                    {
+                    loader: 'css-loader',
+                    options: {
+                        esModule: false,
+                    }
+                }]
             },
             {
                 test: /.png$/,
