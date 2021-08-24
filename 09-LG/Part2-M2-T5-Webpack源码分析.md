9ebbbc24e3022fb857d20a706452c153154c4468
chuhx <chuhx1024@126.com>
2021-08-24 23:31:28 +0800

    webpack t 方法实现

--- 09-LG/Part2-M2-T5-Webpack源码分析.md
+++ 09-LG/Part2-M2-T5-Webpack源码分析.md
@@ -8,4 +8,57 @@
 - 打包后的文件 就相当于 把 {} 传递给了 modules
 
 
+### Webpack 的懒加载实现原理
+- import() 方法可以实现
+```js
+// /*webpackChunkName: 'login'*/ 可以实现自定义 打包 文件
+
+import(/*webpackChunkName: 'login'*/'./login.js').then(login => {  
+    console.log(login)
+})
+```
+- 核心原理就是利用了 jsonp
+
+- 核心方法  __webpack_require__.t 方法
+    - 有个好用的语法 (mode & 1) 
+
+```js
+    /*  
+    *   t 方法内部就做了一件事  就是调用自定义 的 require 方法加载 value 对应模块导出 重新赋值给 value
+    *   t 方法接受参数 value 就是 当前要懒加载的模块ID  mode 就是一个二进制值
+    *   里边的 8 4 ns 2  都是不同的情况 对 value 的加工处理
+    *   (mode & 1) 和 (mode & 8) 成立时  就直接返回 value 此时就是默认的  CommonJS 规范的导出
+    *   (mode & 1) 和 (mode & 4) 成立时  就直接此时就是 ESM 规范的导出
+    *   上述都不成立 就要 用 ns 的相关加工了
+    *       - 如果是普通的 String  就直接挂载
+    *       - 如果是 Object  就要使用 循环的方式挂载了
+    * 
+    */
+	__webpack_require__.t = function(value, mode) {
+ 		if(mode & 1) value = __webpack_require__(value)
+
+ 		if(mode & 8) return value
+
+ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) {
+            return value
+         }
+
+ 		var ns = Object.create(null)
+ 		__webpack_require__.r(ns)
+ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value })
+ 		if(mode & 2 && typeof value != 'string') {
+            for(var key in value) {
+                __webpack_require__.d(ns, key, function(key) { 
+                    return value[key] 
+                }.bind(null, key))
+            }
+        }
+ 		return ns
+ 	}
+
+```
+
+
+
+
 
--- 10-大前端/Webpack/webpack-source-code/dist/mybuilt.js
+++ 10-大前端/Webpack/webpack-source-code/dist/mybuilt.js
@@ -1,8 +1,38 @@
 (function (modules) {
     console.log(modules)
+    	// install a JSONP callback for chunk loading
+ 	function webpackJsonpCallback(data) {
+ 		var chunkIds = data[0]
+ 		var moreModules = data[1]
+ 		// add "moreModules" to the modules object,
+ 		// then flag all "chunkIds" as loaded and fire callback
+ 		var moduleId, chunkId, i = 0, resolves = []
+ 		for(;i < chunkIds.length; i++) {
+ 			chunkId = chunkIds[i]
+ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+ 				resolves.push(installedChunks[chunkId][0])
+ 			}
+ 			installedChunks[chunkId] = 0
+ 		}
+ 		for(moduleId in moreModules) {
+ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+ 				modules[moduleId] = moreModules[moduleId]
+ 			}
+ 		}
+ 		if(parentJsonpFunction) parentJsonpFunction(data)
+
+ 		while(resolves.length) {
+ 			resolves.shift()()
+ 		}
+
+ 	}
     // 定义一个对象缓存加载过的模块
     let installedModules = {}
-    // 定义一个 __webpack_require__ 方法来替换 import require 加载操作
+    // 定义一个 __webpack_modules__ 方法来替换 import require 加载操作
+
+    let installedChunks = {
+        "main": 0
+    }
     function __webpack_modules__ (moduleId) {
         // 检测当前缓存中是否有有被加载的模块内容 有 就返回 没有 就创建
         if (installedModules[moduleId]) {
@@ -37,7 +67,7 @@
         }
     }
     // 定义一个 r 方法 用来标记是 ES6 类型
-    __webpack_modules__.r = function () {
+    __webpack_modules__.r = function (exports) {
         if ( typeof Symbol !== 'undefined' && Symbol.toStringTag) {
             Object.defineProperty(exports, Symbol.toStringTag, {
                 value: 'Module',
@@ -56,6 +86,100 @@
         __webpack_modules__.d(getter, 'a', getter)
         return getter
     }
+    // 懒加载时要用的 方法
+    __webpack_modules__.t = function(value, mode) {
+        if(mode & 1) value = __webpack_modules__(value)
+
+        if(mode & 8) return value
+
+        if((mode & 4) && typeof value === 'object' && value && value.__esModule) {
+           return value
+        }
+
+        var ns = Object.create(null)
+        __webpack_modules__.r(ns)
+        Object.defineProperty(ns, 'default', { enumerable: true, value: value })
+        if(mode & 2 && typeof value != 'string') {
+           for(var key in value) {
+               __webpack_modules__.d(ns, key, function(key) { 
+                   return value[key] 
+               }.bind(null, key))
+           }
+       }
+        return ns
+    }
+    __webpack_modules__.e = function requireEnsure(chunkId) {
+        var promises = []
+
+
+        // JSONP chunk loading for javascript
+
+        var installedChunkData = installedChunks[chunkId]
+        if(installedChunkData !== 0) { // 0 means "already installed".
+
+            // a Promise means "currently loading".
+            if(installedChunkData) {
+                promises.push(installedChunkData[2])
+            } else {
+                // setup Promise in chunk cache
+                var promise = new Promise(function(resolve, reject) {
+                    installedChunkData = installedChunks[chunkId] = [resolve, reject]
+                })
+                promises.push(installedChunkData[2] = promise)
+
+                // start chunk loading
+                var script = document.createElement('script')
+                var onScriptComplete
+
+                script.charset = 'utf-8'
+                script.timeout = 120
+                if (__webpack_modules__.nc) {
+                    script.setAttribute("nonce", __webpack_modules__.nc)
+                }
+                script.src = jsonpScriptSrc(chunkId)
+
+                // create error before stack unwound to get useful stacktrace later
+                var error = new Error()
+                onScriptComplete = function (event) {
+                    // avoid mem leaks in IE.
+                    script.onerror = script.onload = null
+                    clearTimeout(timeout)
+                    var chunk = installedChunks[chunkId]
+                    if(chunk !== 0) {
+                        if(chunk) {
+                            var errorType = event && (event.type === 'load' ? 'missing' : event.type)
+                            var realSrc = event && event.target && event.target.src
+                            error.message = 'Loading chunk ' + chunkId + ' failed.\n(' + errorType + ': ' + realSrc + ')'
+                            error.name = 'ChunkLoadError'
+                            error.type = errorType
+                            error.request = realSrc
+                            chunk[1](error)
+                        }
+                        installedChunks[chunkId] = undefined
+                    }
+                }
+                var timeout = setTimeout(function(){
+                    onScriptComplete({ type: 'timeout', target: script })
+                }, 120000)
+                script.onerror = script.onload = onScriptComplete
+                document.head.appendChild(script)
+            }
+        }
+        return Promise.all(promises)
+    }
+
+    // script path function
+ 	function jsonpScriptSrc(chunkId) {
+        return __webpack_modules__.p + "" + chunkId + ".built.js"
+    }
+    var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || []
+ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray)
+ 	jsonpArray.push = webpackJsonpCallback
+ 	jsonpArray = jsonpArray.slice()
+ 	for(var i = 0; i < jsonpArray.length; i++) {
+        webpackJsonpCallback(jsonpArray[i])
+    }
+ 	var parentJsonpFunction = oldJsonpFunction
 
     //  保存资源得访问路径 如果没有设置 就是 ''
     __webpack_modules__.p = ''
@@ -66,15 +190,18 @@
 (
     {
         "./src/index.js":
-        (function(module, exports, __webpack_require__) {
-        
-            const obtn = document.querySelector('#btn')
-            console.log('我是index.js 的内容')
-            obtn.addEventListener('click', () => {
-                __webpack_require__.e(/*! import() */ 0).then(__webpack_require__.t.bind(null, /*! ./login.js */ "./src/login.js", 7)).then(login => {
-                    console.log(login)
-                })
-            })
+        (function(module, exports, __webpack_modules__) {
+
+          const obtn = document.querySelector('#btn')
+          console.log('我是index.js 的内容')
+
+          obtn.addEventListener('click', () => {
+              __webpack_modules__.e("login")
+              .then(__webpack_modules__.t.bind(null, "./src/login.js", 7))
+              .then(login => {
+                  console.log(login)
+              })
+          })
         })
-    }
+      }
 )
